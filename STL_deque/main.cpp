7d8eb968a2a6a1a9972fedd615c8321684e04342
陈宇峰 <yufengcode@gmail.com>
2016-08-12 10:38:14 +0800

    construct

--- STL_deque/main.cpp
+++ STL_deque/main.cpp
@@ -7,9 +7,55 @@
 //
 
 #include <iostream>
+#include <deque>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void PrintDeque(deque<int> pDeque);
+
+int main(int argc, const char * argv[])
+{
+    deque<int> deque1;
+    deque<int> deque2(deque1);
+    deque<int> deque3(10);
+    deque<int> deque4(10,0);
+
+    deque1.push_back(1);
+    deque1.push_back(2);
+    deque1.push_back(3);
+    deque1.push_back(4);
+    PrintDeque(deque1);
+
+    deque1.push_front(5);
+    deque1.push_front(6);
+    PrintDeque(deque1);
+
+    cout << "头部元素为：" << deque1.front() << endl;
+    cout << "尾部元素为：" << deque1.back() << endl;
+
+
+
     return 0;
 }
+
+void PrintDeque(deque<int> pDeque)
+{
+    cout << "deque元素为：";
+    deque<int>::iterator dequeIter;
+    for (dequeIter = pDeque.begin(); dequeIter != pDeque.end(); dequeIter++)
+    {
+        cout << *dequeIter << " ";
+    }
+
+    cout << endl;
+}
+
+
+
+
+
+
+
+
+
+
